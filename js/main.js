3eedcc5f3b7eb2d9cf84c920c5f08d59511f6c3e
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-08-05 00:11:40 +0600

    home page bottom banner added for desktop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const menuBtn = document.querySelector(".hamburger-btn");
+const sideMenuWrapper = document.querySelector(".sideMenu-wrapper");
 const sideMenu = document.querySelector(".side-menu");
+const mainWrapper = document.querySelector(".main-wrapper");
+const siteContent = document.querySelector(".site-content");
 
 menuBtn.addEventListener("click", () => {
-  if (sideMenu.classList.contains("menu-active")) {
+  if (
+    sideMenu.classList.contains("menu-active") ||
+    sideMenuWrapper.classList.contains("menu-active")
+  ) {
     sideMenu.classList.remove("menu-active");
+    mainWrapper.classList.remove("menu-active");
+    siteContent.classList.remove("menu-active");
+    sideMenuWrapper.classList.remove("menu-active");
   } else {
     sideMenu.classList.add("menu-active");
+    sideMenuWrapper.classList.add("menu-active");
+    mainWrapper.classList.add("menu-active");
+    siteContent.classList.add("menu-active");
   }
 });
